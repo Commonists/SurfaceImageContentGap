e5b982a65e7d4e67fc1afe41ae9b7a6b8ccd6f3a
Pierre-Selim <pierre-selim.huard@wikimedia.fr>
2015-05-25 10:06:29 +0200

    Calling reporting module in main script. Closes #2

--- surfaceimagecontentgap/sicg.py
+++ surfaceimagecontentgap/sicg.py
@@ -3,10 +3,13 @@
 
 """Surface image content gap."""
 
+from ConfigParser import RawConfigParser
 import logging
 
 import mwclient
 import requests
+
+import report
 
 
 # logger
@@ -16,7 +19,7 @@ LOG = logging.getLogger()
 WIKIPEDIA_URL = '{0}.wikipedia.org'
 PROTOCOL = 'https'
 USER_AGENT = 'Bot based on mwclient'
-GROK_SE_URL = "http://stats.grok.se/json/{0:s}/getlatest{1:d}/{2:s}"
+GROK_SE_URL = "http://stats.grok.se/json/{0:s}/latest{1:d}/{2:s}"
 
 
 def isthereanimage(article):
@@ -31,7 +34,7 @@ def isthereanimage(article):
 
 
 def getlatest(article, latest):
-    """ Returns amount of views from the getlatest days
+    """ Returns amount of views from the latest days
 
     Args:
         article (article): article on which we are querying stats.
@@ -49,6 +52,7 @@ def getlatest(article, latest):
     url = GROK_SE_URL.format(article.site.site['lang'],
                              latest,
                              article.name.encode('utf-8'))
+    LOG.debug('\tRequest to %s', url)
     result = requests.get(url).json()
     if 'daily_views' in result:
         return sum([result['daily_views'][d] for d in result['daily_views']])
@@ -62,6 +66,9 @@ def crawlcategory(language, category):
     Args:
         language (str): language code of wikipedia
         category (str): name of the category to crawl.
+
+    Returns
+        list: containing dictionnaries with the article name and the total view
     """
     site = mwclient.Site((PROTOCOL, WIKIPEDIA_URL.format(language)),
                          clients_useragent=USER_AGENT)
@@ -70,14 +77,36 @@ def crawlcategory(language, category):
     LOG.info("Found: %s articles", len(articles))
     for article in articles:
         if not isthereanimage(article):
-            noimagearticles.append((article, getlatest(article, 90)))
+            noimagearticles.append({'name': article.name.encode('utf-8'),
+                                    'views': getlatest(article, 90)})
             LOG.info("\tNo image found in: %s", article.name.encode('utf-8'))
     LOG.info("Finished, found %s articles without images out of %s",
              len(noimagearticles),
              len(articles))
     sorted_result = sorted(noimagearticles,
-                           key=lambda x: x[1])
+                           key=lambda x: x['views'])
     return sorted_result
+
+
+def readconfig(configname):
+    """Read the config file.
+
+    Args:
+        configname (str): configuration file name. Should have section login
+            and entry for user and password.
+
+    Returns:
+        dict: with user and password key.
+
+    Raises:
+        ValueError: when configname is None
+    """
+    if configname is None:
+        raise ValueError('Config file should be provided with -f/--configfile')
+    configparser = RawConfigParser()
+    configparser.read(configname)
+    return {'user': configparser.get('login', 'user'),
+            'password': configparser.get('login', 'password')}
 
 
 def setuplog():
@@ -96,27 +125,43 @@ def setuplog():
 
 
 def main():
-    """ main script. """
+    """main function."""
     setuplog()
     from argparse import ArgumentParser
     description = 'Analyzing Wikipedia to surface image content gap.'
     parser = ArgumentParser(description=description)
-    parser.add_argument("-c", "--category",
+    parser.add_argument('-c', '--category',
                         type=str,
-                        dest="category",
+                        dest='category',
                         required=False,
                         default='Portail:Informatique théorique/Articles liés',
-                        help="Article category on wikipedia")
-    parser.add_argument("-w", "--wikipedia",
+                        help='Article category on wikipedia')
+    parser.add_argument('-w', '--wikipedia',
                         type=str,
-                        dest="lang",
+                        dest='lang',
                         required=True,
-                        help="Language code for Wikipedia")
+                        help='Language code for Wikipedia')
+    parser.add_argument('-r', '--report',
+                        type=str,
+                        dest='report',
+                        required=False,
+                        default=None,
+                        help='Optional, page name to write a report.')
+    parser.add_argument('-f', '--configfile',
+                        type=str,
+                        dest='config',
+                        required=False,
+                        default=None,
+                        help='Optional, config file with login and password.')
     args = parser.parse_args()
     result = crawlcategory(args.lang, args.category)
-    for (article, viewcount) in result:
-        print "{0}\t\t{1}".format(article.name, viewcount)
-
+    if args.report is not None:
+        conf = readconfig(args.config)
+        site = mwclient.Site((PROTOCOL, WIKIPEDIA_URL.format(args.lang)),
+                             clients_useragent=USER_AGENT)
+        site.login(conf['user'], conf['password'])
+        myreport = report.create(result)
+        report.save(site, args.report, myreport)
 
 if __name__ == '__main__':
     main()
