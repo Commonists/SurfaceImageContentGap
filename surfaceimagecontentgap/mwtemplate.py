2acfde31e66137998ca69605a5a41dfb372bcdbf
Pierre-Selim <pierre-selim.huard@wikimedia.fr>
2015-07-19 11:17:31 -0500

    The mwclient way to do the query
    Rewrite the API call to use mwclient.listing module.

--- surfaceimagecontentgap/mwtemplate.py
+++ surfaceimagecontentgap/mwtemplate.py
@@ -6,6 +6,7 @@ import logging
 
 
 import mwclient
+import mwclient.listing as listing
 
 LOGGER_NAME = 'sicglog'
 LOG = logging.getLogger(LOGGER_NAME)
@@ -24,47 +25,15 @@ class ArticleWithTemplate(object):
 
     def listarticles(self):
         """List of articles containing a given template."""
-        result = []
-        has_continue = True
-        eicontinue = None
-        while has_continue:
-            LOG.info("Searching for article with template...size %d %s",
-                     len(result),
-                     eicontinue)
-            api_result = self.queryapi(eicontinue)
-            result += self.parse_result(api_result)
-            if 'query-continue' in api_result:
-                has_continue = True
-                eicontinue = self.parse_eicontinue(api_result)
-            else:
-                has_continue = False
-        return result
-
-    def queryapi(self, eicontinue):
-        """Send the query to wikipedia."""
-        if eicontinue is None:
-            return self.site.api('query',
-                                 list='embeddedin', rawcontinue='',
-                                 eilimit='max',
-                                 eititle=self.templatename)
-        else:
-            return self.site.api('query', eicontinue=eicontinue,
-                                 list='embeddedin', rawcontinue='',
-                                 eilimit='max',
-                                 eititle=self.templatename)
-
-    @staticmethod
-    def parse_eicontinue(api_result):
-        """The eicontinue of the api_result."""
-        return api_result['query-continue']['embeddedin']['eicontinue']
-
-    def parse_result(self, api_result):
-        """Returns list of articles from result of api."""
-        if 'query' in api_result:
-            if 'embeddedin' in api_result['query']:
-                return [mwclient.page.Page(self.site, a['title'])
-                        for a in api_result['query']['embeddedin']]
-            else:
-                return []
-        else:
-            return []
+        kwargs = dict(listing.List.generate_kwargs('ei', prop='title',
+                                                   title=self.templatename))
+        gen = listing.List(self.site, 'embeddedin', 'ei', **kwargs)
+        articles = []
+        for info in gen:
+            titlename = info['title']
+            articles.append(mwclient.page.Page(self.site, titlename))
+            if len(articles) % 100 == 0:
+                LOG.info("Fetch %d articles with template %s",
+                         len(articles),
+                         self.templatename)
+        return articles
