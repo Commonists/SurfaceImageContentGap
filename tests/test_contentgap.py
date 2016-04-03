3f4b1d7149e44ef85fea32eac38c9320d3fe6057
Jean-Fred <JeanFred@github>
2016-04-03 13:29:28 +0100

    Add unit test setup with coverage
    - Add requirements-test file
    - Add environment to tox
    - Add various config files
    - Add very barebone unittest file
    - Add execution via Travis-CI

---
+++ tests/test_contentgap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+"""Unit tests."""
+
+import unittest
+from surfaceimagecontentgap import contentgap
+
+
+class Test(unittest.TestCase):
+
+    def test_init_contentgap_with_empty_collection(self):
+        gap = contentgap.ContentGap([])
+        self.assertEquals(gap.articles, [])
+
+if __name__ == "__main__":
+    unittest.main()
